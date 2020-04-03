464896d53c8d3ee001455ee2bda953be255ed591
Tarun Patel <922168@my.leicestercollege.ac.uk>
2020-04-03 21:54:51 +0100

    Final commit

---
+++ GamesReview/GamesReview/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GamesReview.Models
+{
+    public class Product
+    {
+        public virtual int ProductId { get; set; }
+        public virtual string GameName { get; set; }
+        public virtual DateTime PubDate { get; set; }
+        public virtual string Developer { get; set; }
+        public virtual int StarRating { get; set; }
+
+    }
+
+}
+
+
+
+        
