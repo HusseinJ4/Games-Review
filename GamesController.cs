09ee34ce7ef007b9d9a4d7741223ef1dd2466fe2
Ranitha1 <59957886+Ranitha1@users.noreply.github.com>
2020-04-03 22:55:30 +0100

    Add files via upload

---
+++ GamesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GamesReview.Data;
+using GamesReview.Models;
+
+namespace GamesReview.Controllers
+{
+    public class GamesController : Controller
+    {
+        private GamesReviewContext db = new GamesReviewContext();
+
+        // GET: Games
+        public ActionResult Index()
+        {
+            return View(db.Games.ToList());
+        }
+
+        // GET: Games/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Games games = db.Games.Find(id);
+            if (games == null)
+            {
+                return HttpNotFound();
+            }
+            return View(games);
+        }
+
+        // GET: Games/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Games/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "GamesId,Name,Developer,Genre,Rating,ReleaseYear,Summary")] Games games)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Games.Add(games);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(games);
+        }
+
+        // GET: Games/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Games games = db.Games.Find(id);
+            if (games == null)
+            {
+                return HttpNotFound();
+            }
+            return View(games);
+        }
+
+        // POST: Games/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "GamesId,Name,Developer,Genre,Rating,ReleaseYear,Summary")] Games games)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(games).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(games);
+        }
+
+        // GET: Games/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Games games = db.Games.Find(id);
+            if (games == null)
+            {
+                return HttpNotFound();
+            }
+            return View(games);
+        }
+
+        // POST: Games/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Games games = db.Games.Find(id);
+            db.Games.Remove(games);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
