c8aa3aba9c1f44d25e7e2f798c7ab322c3bde404
Ranitha1 <59957886+Ranitha1@users.noreply.github.com>
2020-04-03 22:56:20 +0100

    Add files via upload

---
+++ Games.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GamesReview.Models
+{
+    public class Games
+    {
+
+        public virtual int GamesId { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Developer { get; set; }
+        public virtual string Genre { get; set; }
+        public virtual string Rating { get; set; }
+        public virtual Int32 ReleaseYear { get; set; }
+        public virtual string Summary { get; set; }
+    }
+}
